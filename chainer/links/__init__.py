9a05689be9aeaa5ed1e9d3bc306b0ccb2e0d7024
Yuya Unno <unnonouno@gmail.com>
2016-04-08 23:01:49 +0900

    Implement MGU

--- chainer/links/__init__.py
+++ chainer/links/__init__.py
@@ -11,6 +11,7 @@ from chainer.links.connection import inception
 from chainer.links.connection import inceptionbn
 from chainer.links.connection import linear
 from chainer.links.connection import lstm
+from chainer.links.connection import mgu
 from chainer.links.connection import mlp_convolution_2d
 from chainer.links.connection import parameter
 from chainer.links.loss import hierarchical_softmax
@@ -32,6 +33,8 @@ Inception = inception.Inception
 InceptionBN = inceptionbn.InceptionBN
 Linear = linear.Linear
 LSTM = lstm.LSTM
+MGU = mgu.MGU
+StatefulMGU = mgu.StatefulMGU
 MLPConvolution2D = mlp_convolution_2d.MLPConvolution2D
 Parameter = parameter.Parameter
 
